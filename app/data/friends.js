e7b5cce837d87367a93245405a28a2af6a4ba399
quasimodeaux <gatchey@yahoo.com>
2017-06-29 09:30:27 -0400

    final still need to finish css + calc bugs

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+// Friends
+// =============================================================
+var friends = [{
+    name: "kevin",
+    photo: "",
+    scores: [ 5, 1, 3, 3, 3, 3, 3, 3, 3, 1 ]
+}, {
+    name: "Monica",
+    photo: "",
+    scores: [ 2, 4, 4, 4, 2, 1, 2, 4, 2, 1 ]
+}, {
+    name: "Hanzo",
+    photo: "",
+    scores: [ 3, 1, 2, 2, 5, 1, 2, 5, 4, 1 ]
+}, {
+    name: "Jessie",
+    photo: "",
+    scores: [ 5, 5, 1, 4, 2, 1, 2, 5, 4, 1 ]
+    }];
+
+module.exports = friends;
---
+++ app/public/survey.js
@@ -0,0 +1,15 @@
+// Calculations for matching scores
+
+
+// set up answers array or initialize to zero
+
+
+
+// onclick functions for drop down menus to equal their value
+        // save user info object on click as well
+
+
+
+//Finish Modal along with answers.
+
+//
