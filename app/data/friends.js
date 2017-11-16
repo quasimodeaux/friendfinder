e6dc6e0675a76ad0f373dc7a0323f9be94dd58c0
quasimodeaux <gatchey@yahoo.com>
2017-11-16 12:09:03 -0500

    final

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,31 @@
 // Friends
 // =============================================================
 var friends = [{
-    name: "kevin",
-    photo: "",
+    name: "Roadhog",
+    photo: "https://i.ytimg.com/vi/pOTPJW9UGeg/maxresdefault.jpg",
     scores: [ 5, 1, 3, 3, 3, 3, 3, 3, 3, 1 ]
 }, {
-    name: "Monica",
-    photo: "",
+    name: "Mercy",
+    photo: "https://vignette.wikia.nocookie.net/overwatch/images/c/cd/Mercy_portrait.png/revision/latest/scale-to-width-down/350?cb=20160429042205",
     scores: [ 2, 4, 4, 4, 2, 1, 2, 4, 2, 1 ]
 }, {
     name: "Hanzo",
-    photo: "",
-    scores: [ 3, 1, 2, 2, 5, 1, 2, 5, 4, 1 ]
+    photo: "https://vignette.wikia.nocookie.net/overwatch/images/a/aa/Hanzo_Concept.jpg/revision/latest/scale-to-width-down/171?cb=20160502184344",
+    scores: [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
 }, {
-    name: "Jessie",
-    photo: "",
+    name: "Mcree",
+    photo: "https://funnypictures2.fjcdn.com/funny_gifs/Mcree+the+heart+stopper+rip+mcree+next+patch_5b372f_5944660.gif",
     scores: [ 5, 5, 1, 4, 2, 1, 2, 5, 4, 1 ]
+    }, {
+
+    name: "Reinhart",
+    photo: "https://static.invenglobal.com/upload/image/2017/03/12/i1489378387953872.jpeg",
+    scores: [ 3, 1, 2, 2, 5, 1, 2, 5, 4, 1 ]
+    },
+    {
+        name: "D.Va",
+        photo: "http://orcz.com/images/thumb/5/52/OverwatchDVa.jpg/400px-OverwatchDVa.jpg",
+        scores: [ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ]
     }];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,10 +7,41 @@ module.exports = function(app) {
     });
 
     app.post('/api/friends', function(req, res) {
+        var match = {
+            name: "",
+            photo:"",
+            friendDifference: 1000
+        };
 
-        friends.push(req.body);
+        var userData = req.body;
+        console.log('userData', userData);
+        var userScores = userData.scores;
+
+
+
+        var totalDifference = 0;
+
+        for (var i =0; i < friends.length; i++){
+
+            console.log(friends[i].name);
+            totalDifference = 0;
+            for (var j = 0; j <friends[i].scores[j]; j++){
+            totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+
+            if (totalDifference <= match.friendDifference){
+
+                match.name = friends[i].name;
+                match.photo = friends[i].photo;
+                match.friendDifference = totalDifference;
+            }
+
+            }
+        }
+
+        friends.push(userData);
         console.log(friends);
 
-        res.json(friends);
+        res.json(match);
     });
 }
